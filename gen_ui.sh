8bc66f8db34224d726dcd1891fa1ba7ed8600ecc
Paul RASCLE <paul.rascle@openfields.fr>
2024-01-17 19:16:33 +0100

    inheritance ui

---
+++ gen_ui.sh
@@ -0,0 +1,1 @@
+pyside2-uic main.ui > ui_mainwindow.py
--- mainGui.py
+++ mainGui.py
@@ -1,51 +1,34 @@
 
 import sys
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication
+from PySide2.QtWidgets import QApplication, QMainWindow
 from PySide2.QtCore import QFile, QIODevice
+from ui_mainwindow import Ui_MainWindow
+
+class VATGui(QMainWindow, Ui_MainWindow):
+    """
+    Python Qt GUI for VAT_class
+    """
+
+    def __init__(self):
+        QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+
+        self.tbw.setTabText(0, "Target overview")
+        self.tbw.setTabText(1, "Fits contents specifications")
+        self.tbw.setTabText(2, "Download data")
+        for i in range(self.tbw.count()):
+            self.tbw.setTabEnabled(i, False)
+        self.tbw.setTabEnabled(0, True)
+        self.tbw.setCurrentIndex(0)
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    ui_file_name = "main.ui"
-    ui_file = QFile(ui_file_name)
-    if not ui_file.open(QIODevice.ReadOnly):
-        print("Cannot open {}: {}".format(ui_file_name, ui_file.errorString()))
-        sys.exit(-1)
-    loader = QUiLoader()
-    window = loader.load(ui_file)
-    ui_file.close()
-    if not window:
-        print(loader.errorString())
-        sys.exit(-1)
-    window.tbw.setTabText(0, "Target overview")
-    window.tbw.setTabText(1, "Fits contents specifications")
-    window.tbw.setTabText(2, "Download data")
-    for i in range(window.tbw.count()):
-        window.tbw.setTabEnabled(i, False)
-    window.tbw.setTabEnabled(0, True)
-    window.tbw.setCurrentIndex(0)
-
+    window = VATGui()
     window.show()
 
     sys.exit(app.exec_())
-
-# ---------------------------------------------------
-import sys
-from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication
-from PySide2.QtCore import QFile, QIODevice
-
-app = QApplication()
-
-ui_file_name = "main.ui"
-ui_file = QFile(ui_file_name)
-loader = QUiLoader()
-window = loader.load(ui_file)
-ui_file.close()
-window.tbw.setTabText(0, "Target overview")
-window.tbw.setTabText(1, "Fits contents specifications")
-window.tbw.setTabText(2, "Download data")
-window.show()
-
-sys.exit(app.exec_())
