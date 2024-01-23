70b491ef3922feb3cd7f1d75b6b9a9af5d361ce0
Paul RASCLE <paul.rascle@openfields.fr>
2024-01-23 18:37:16 +0100

    decimal point

--- launch.sh
+++ launch.sh
@@ -4,7 +4,7 @@ CONDA_ROOT=$(realpath "$(dirname $(which conda))/..")
 . ${CONDA_ROOT}/etc/profile.d/conda.sh                                       # required to have access to conda commands in a shell script
 
 conda activate astro
-
+export LANG='C'
 pyside2-uic main.ui > ui_mainwindow.py
 
 ./mainGui.py
